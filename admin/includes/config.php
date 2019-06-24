1fc5c8f42fd3d9f16dbabb8c2468f123761176b1
YukitoSaka <hoangdh912@gmail.com>
2019-06-24 18:30:13 +0700

    user class done

---
+++ admin/includes/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+//Database Constants
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','123456');
+define('DB_NAME','gallery_db');
+
+
+
---
+++ admin/includes/database.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once('config.php');
+
+class Database {
+
+	public $connection;
+
+	public function __construct(){
+		$this->open_db_connection();
+	}
+
+	public function open_db_connection(){
+		//$this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		$this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		if($this->connection->connect_errno){
+			die("Database connection failed" . $this->connection->connect_error);
+		}
+	}
+	
+	public function query($sql){
+		$result = $this->connection->query($sql);	
+		$this->confirm_query($result);
+		return $result;
+	}
+
+	//Helper Method
+	private function confirm_query($result){
+		if(!$result){
+			die("Query Failed" . $this->connection->error);
+		}
+	}
+
+	public function escape_string($string){
+		$escape_string = $this->connection->real_escappe_string($string);
+		return $escape_string;
+	}
+
+	public function the_insert_id(){
+		return $this->connection->insert_id;
+	}
+}
+
+$database = new Database();
+
+
+
+
+
