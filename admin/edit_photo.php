fbaa0e50b69a48b996f93df332cc0cb7d33948c2
hoang-dinh <https://proto-itsd.backlog.jp>
2019-07-05 11:18:21 +0700

    Dislay edit done

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -4,7 +4,22 @@
 
 <?php
 
+if(empty($_GET['id'])){
+  redirect("photos.php");
+} else {
+  $photo = Photo::find_by_id($_GET['id']);
 
+  if(isset($_POST['update'])){
+    if($photo){
+      $photo->title = $_POST['title'];
+      $photo->caption = $_POST['caption'];
+      $photo->alternate_text = $_POST['alternate_text'];
+      $photo->description = $_POST['description']; Add caption and alternate_text to PHPmyAdmin
+
+
+    }
+  }
+}
 
  ?>
         <!-- Navigation -->
@@ -35,31 +50,70 @@
                             <small>Subheading</small>
                         </h1>
 
-                        <div class="col-md-8">
-                          <div class="form-group">
+                        <form class="" action="" method="post">
 
-                            <input type="text" name="title"class="form-control" >
+                          <div class="col-md-8">
+                            <div class="form-group">
 
+                              <input type="text" name="title" class="form-control" value="<?php echo $photo->title; ?>" >
+
+                            </div>
+
+                            <div class="form-group">
+                              <label for="caption">Caption</label>
+                              <input type="text" name="caption" class="form-control" value="<?php echo $photo->caption; ?>" >
+
+                            </div>
+
+                            <div class="form-group">
+                              <label for="caption">Alternate Text</label>
+                              <input type="text" name="alternate_text" class="form-control" value="<?php echo $photo->alternate_text; ?>" >
+
+                            </div>
+
+                            <div class="form-group">
+                              <label for="caption">Description</label>
+                              <textarea class= "form-control" name="description" rows="10" cols="30" ><?php echo $photo->description; ?>"</textarea>
+
+                            </div>
                           </div>
 
-                          <div class="form-group">
-                            <label for="caption">Caption</label>
-                            <input type="text" name="caption"class="form-control" >
-
+                          <div class="col-md-4" >
+                              <div  class="photo-info-box">
+                                  <div class="info-box-header">
+                                     <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                                  </div>
+                              <div class="inside">
+                                <div class="box-inner">
+                                   <p class="text">
+                                     <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                                    </p>
+                                    <p class="text ">
+                                      Photo Id: <span class="data photo_id_box">34</span>
+                                    </p>
+                                    <p class="text">
+                                      Filename: <span class="data">image.jpg</span>
+                                    </p>
+                                   <p class="text">
+                                    File Type: <span class="data">JPG</span>
+                                   </p>
+                                   <p class="text">
+                                     File Size: <span class="data">3245345</span>
+                                   </p>
+                                </div>
+                                <div class="info-box-footer clearfix">
+                                  <div class="info-box-delete pull-left">
+                                      <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>
+                                  </div>
+                                  <div class="info-box-update pull-right ">
+                                      <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
                           </div>
 
-                          <div class="form-group">
-                            <label for="caption">Alternate Text</label>
-                            <input type="text" name="alternate_text"class="form-control" >
-
-                          </div>
-
-                          <div class="form-group">
-                            <label for="caption">Description</label>
-                            <textarea class= "form-control" name="description" rows="10" cols="30"></textarea>
-
-                          </div>
-                        </div>
+                        </form>
 
                     </div>
                 </div>
--- admin/photos.php
+++ admin/photos.php
@@ -54,7 +54,7 @@
                                     <div class="pictures_link">
 
                                       <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
-                                      <a href="#">Edit</a>
+                                      <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
                                       <a href="#">View</a>
 
                                     </div>
