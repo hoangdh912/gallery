ae70c24d2295f8767c81e72d6aef86ebcde85ab6
YukitoSaka <hoangdh912@gmail.com>
2019-07-13 16:35:50 +0700

    Done Admin comments

--- admin/comment_photo.php
+++ admin/comment_photo.php
@@ -3,8 +3,14 @@
 <?php if(!$session->is_signed_in()){ redirect("login.php");} ?>
 
 <?php
+  
+  if(empty($_GET['id'])){
+    redirect("photos.php");
+  }
 
-  $photos = Photo::find_all();
+  $comments = Comment::find_the_comments($_GET['id']);
+
+
 
  ?>
         <!-- Navigation -->
@@ -31,51 +37,37 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Photos
-                            <small>Subheading</small>
+                            Users
                         </h1>
+
+                        <a href="add_user.php" class="btn btn-primary">Add User</a>
 
                         <div class="col-md-12">
                           <table class="table table-hover">
                             <thead>
                               <tr>
-                                <th>Photo</th>
-                                <th>ID</th>
-                                <th>File Name</th>
-                                <th>Title</th>
-                                <th>Size</th>
-                                <th>Comments</th>
+                                <th>Id</th>
+                                <th>Author</th>
+                                <th>Body</th>
                               </tr>
                             </thead>
 
                             <tbody>
-                              <?php foreach ($photos as $photo) : ?>
+                              <?php foreach ($comments as $comment) : ?>
                                 <tr>
-                                  <td><img src="<?php echo $photo->picture_path(); ?>" alt="" class="admin-photo-thumbnail">
+                                  <td><?php echo $comment->id; ?></td>  
+                                  
+                                  <td><?php echo $comment->author; ?>
                                     <div class="action_link ">
 
-                                      <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
-                                      <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
-                                      <a href="../photo.php?id=<?php echo $photo->id; ?>">View</a>
+                                      <a href="delete_comment_photo.php?id=<?php echo $comment->id; ?>">Delete</a>
 
                                     </div>
 
                                   </td>
-                                  <td><?php echo $photo->id; ?></td>
-                                  <td><?php echo $photo->filename; ?></td>
-                                  <td><?php echo $photo->title; ?></td>
-                                  <td><?php echo $photo->size; ?></td>
-                                  <td>
-                                    <a href="comment_photo.php?id=<?php echo $photo->id; ?>">
-                                      <?php 
-
-                                      $comments = Comment::find_the_comments($photo->id);
-
-                                      echo count($comments); 
-
-                                      ?>
-                                    </a>
-                                  </td>
+                                  
+                                  <td><?php echo $comment->body; ?></td>
+                             
                                 </tr>
                             <?php endforeach; ?>
                             </tbody>
